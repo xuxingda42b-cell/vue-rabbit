0529f9ad96cd30bb60f0d64f92d40673086085ae
QxAxQ <xuxingda42b@gmail.com>
2026-03-16 22:05:26 +0800

    axios基础封装

---
+++ src/utils/http.js
@@ -0,0 +1,27 @@
+// axios基础的封装
+import axios from 'axios'
+
+const httpInstance = axios.create({
+  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  timeout: 5000
+})
+
+// 添加请求拦截器
+httpInstance.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+httpInstance.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+export default httpInstance
