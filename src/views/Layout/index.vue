d6143e84de0dc3ecbd8c9e6d670b11740327d547
QxAxQ <xuxingda42b@gmail.com>
2026-03-18 21:59:33 +0800

    Revert "静态结构搭建"
    This reverts commit 8f005de764ce20b00febb7b69bc0bc949d89e173.

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,12 +1,7 @@
-<script setup>
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
-import LayoutFooter from './components/LayoutFooter.vue'
-</script>
-
 <template>
-  <LayoutNav />
-  <LayoutHeader />
-  <RouterView />
-  <LayoutFooter />
+  <div>
+    我是首页
+    <!-- 二级路由出口 -->
+    <RouterView />
+  </div>
 </template>
