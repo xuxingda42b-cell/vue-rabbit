25f6b2e320cea5170bfccaa6f79895ddf8b87d2f
QxAxQ <xuxingda42b@gmail.com>
2026-03-29 21:44:55 +0800

    home-懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useIntersectionObserver } from '@vueuse/core'
 
 //引入初始化样式文件
 import '@/styles/common.scss'
@@ -12,3 +13,23 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令
+
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    //el:指令绑定的那个元素 img
+    //binding: binging.value 指令等于号后面绑定的表达式的值 图片url
+    // console.log(el, binding);
+    useIntersectionObserver(
+      el,
+      ([entry]) => {
+        // console.log(entry);
+        if (entry) {
+          //进入视口区域
+          el.src = binding.value
+        }
+      },
+    )
+  }
+})
