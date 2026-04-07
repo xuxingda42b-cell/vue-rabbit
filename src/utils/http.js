44ec2bd633c9356103a34a0cc93f9d059ccd9859
QxAxQ <xuxingda42b@gmail.com>
2026-04-07 18:40:54 +0800

    登录-基础业务实现

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 // axios基础的封装
 import axios from 'axios'
+import { ElMessage } from 'element-plus';
+import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -21,7 +23,14 @@ httpInstance.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
   // 对响应错误做点什么
+
+  ElMessage({
+    type: 'warning',
+    message: error.response.data.message
+  })
+
   return Promise.reject(error);
+
 });
 
 export default httpInstance
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue';
-
-
+import {loginAPI} from '@/apis/user'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router';
 
 // 表单校验（账号名 + 密码）
 // 1. 准备表单对象
@@ -50,14 +52,22 @@ const rules = {
 
 // 3. 获取form实例做统一校验
 const formRef = ref(null)
-const doLogin = ()=>{
+const router = useRouter()
+const doLogin = async()=>{
+  const {account,password} = form.value
   //调用实例方法
-  formRef.value.validate((valid) =>{
+  formRef.value.validate(async (valid) =>{
     //valid：所有表单都通过校验 才为true
     console.log(valid);
     //以valid作为判断条件 如果通过校验才执行登录逻辑
     if(valid){
       //TODO LOGIN
+      const res = await loginAPI({account,password})
+      console.log(res);
+      //1. 提示用户
+      ElMessage({type:'success',message:'登录成功'})
+      //2. 跳转首页
+      router.replace({path:'/'})
     }
   })
 }
