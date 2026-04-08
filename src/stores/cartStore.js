d207b7bc1372fb2af76232994a547d0456e1850d
QxAxQ <xuxingda42b@gmail.com>
2026-04-08 21:28:27 +0800

    头部购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -21,9 +21,19 @@ export const useCartStore = defineStore('cart', () => {
       cartList.value.push(goods)
     }
   }
+
+  //删除购物车
+  const delCart = (skuId) => {
+    //思路：
+    //1. 找到要删除的下标值 - splice
+    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+    cartList.value.splice(idx, 1)
+    //2. 使用数组的过滤方法 - filter
+  }
   return {
     cartList,
-    addCart
+    addCart,
+    delCart
   }
 }, {
   persist: true,
