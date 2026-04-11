aca34e220b3f3de0e7f30cbb40958de3b3c45f9b
QxAxQ <xuxingda42b@gmail.com>
2026-04-11 19:08:51 +0800

    支付页面-支付结果的展示

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -15,7 +15,7 @@ onMounted(()=>getPayInfo())
 //携带订单id以及回调地址跳转到支付地址（get）
 // 支付地址
 const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
-const backURL = 'http://127.0.0.1:5173/paycallback'
+const backURL = 'http://Localhost:5173/paycallback'
 const redirectUrl = encodeURIComponent(backURL)
 const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
